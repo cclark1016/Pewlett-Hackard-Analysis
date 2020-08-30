d62e8f6057968f429f9e8bc8754563cfe3e9926f
Cristobal <chrisclark1016@gmail.com>
2020-08-30 12:50:04 -0400

    Final submission upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,3 +30,25 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(emp_no) DESC;
+
+SELECT DISTINCT ON(e.emp_no)  e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	tt.title
+--INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as tt
+ON e.emp_no = tt.emp_no
+WHERE(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+SELECT COUNT(emp_no)
+from unique_titles;
+
+SELECT COUNT(emp_no)
+from mentorship_eligibility;
